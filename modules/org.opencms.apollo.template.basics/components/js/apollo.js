4e5961177501b89ec7c25c4c07d01c3fe957b26f
mbuttlies <m.buttlies@alkacon.com>
2015-12-09 12:11:38 +0100

    Fixed navigation for smaller screens. (SCSS, JS)

--- modules/org.opencms.apollo.template.basics/components/js/apollo.js
+++ modules/org.opencms.apollo.template.basics/components/js/apollo.js
@@ -152,14 +152,14 @@ $(document)
 									});
 				});
 
-//$('.dropdown').click(function() {
-//	if ($(this).hasClass('active')) {
-//		$(this).removeClass('active');
-//	} else {
-//		$('.dropdown.active').removeClass('active');
-//		$(this).addClass('active');
-//		$('html, body').animate({
-//			scrollTop : $(this).offset().top - 100
-//		}, 1000);
-//	}
-//});
+$('.dropdown').click(function() {
+	if ($(this).hasClass('active')) {
+		$(this).removeClass('active');
+	} else {
+		$('.dropdown.active').removeClass('active');
+		$(this).addClass('active');
+		$('html, body').animate({
+			scrollTop : $(this).offset().top - 100
+		}, 1000);
+	}
+});
