2ae8c3445bdc00382d3eae721c226d728eba503a
aKandzior <a.kandzior@alkacon.com>
2016-04-27 18:50:56 +0200

    Unified grunt build process.
    Added source maps to Apollo Template CSS / JS build.

--- grunt-opencms-modules.js
+++ grunt-opencms-modules.js
@@ -1,5 +1,5 @@
 
-var modules = [];
+var modules = {};
 
 var templates = [];
 var sassSrc = [];
@@ -12,6 +12,10 @@ var templateCssSrc = [];
 var templateConcatTasks = [];
 
 var baseTemplateCssSrc;
+var deployTarget;
+
+var grunt;
+var buildBase;
 
 exports.loadModule = function(moduleName) {
 	
@@ -19,24 +23,226 @@ exports.loadModule = function(moduleName) {
 	try {
 		var m = require(f);
 		
-		modules.push(m);
+		modules[m.module] = m;
 
-		exports.templates = templates = jsSrc.concat(m.templates);
-		exports.sassSrc = sassSrc = sassSrc.concat(m.sassSrc);
-		exports.cssSrc = cssSrc = cssSrc.concat(m.cssSrc);
-		exports.jsSrc = jsSrc = jsSrc.concat(m.jsSrc);
-		exports.resources = resources = resources.concat(m.resources);
+		sassSrc = sassSrc.concat(m.sassSrc);
+		cssSrc = cssSrc.concat(m.cssSrc);
+		jsSrc = jsSrc.concat(m.jsSrc);
+		resources = resources.concat(m.resources);
+		templates = templates.concat(m.templates);
+		
 		exports.baseTemplateCssSrc = baseTemplateCssSrc = m.baseTemplateCssSrc;
+		exports.deployTarget = deployTarget = m.deployTarget;
 
 		return m;
 	} catch (err) {
 		console.error('Error: ' + err.message);
 	}
-},
+}
 
-exports.showImports = function() {
+exports.initGrunt = function(_grunt, _buildBase) {
 	
-	console.log('\nLoaded path elements from OpenCms modules:');
+	grunt = _grunt;
+	buildBase = _buildBase;
+	
+	_gruntLoadNpmTasks();
+	_gruntInitConfig();
+	_gruntRegisterTasks();
+		
+	if (grunt.option('verbose')) {
+		_showImports();
+	}	
+}
+
+_gruntLoadNpmTasks = function() {
+
+	// These plugins provide necessary tasks
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+}
+
+_gruntInitConfig = function() {
+
+	// Project configuration
+	grunt.initConfig({
+			
+		clean : {
+			all : [ buildBase + '**' ],
+			template : {
+				files : [ {
+					expand : true,
+					src : [ '*.css', '*.css.map' ],
+					filter : 'isFile',
+				} ],
+			},
+		},
+		
+		copy : {
+			pluginDeps : {
+				files : [ {
+					expand : true,
+					src : oc.resources(),
+					dest : buildBase + '03_final/css',
+					flatten : true,
+					filter : 'isFile'
+				} ],
+			},
+			template : {
+				files : [ {
+					expand : true,
+					src : [ '*.css', '*.css.map' ],
+					dest : buildBase + '02_minified',
+					filter : 'isFile',
+				} ],
+			},
+			deploy : {
+				files : [ {
+					expand : true,
+					cwd : buildBase + '03_final/',
+					src : [ '**' ],
+					dest : oc.deployTarget,
+				} ]
+			}
+		},
+		
+		sass : {
+			template : {
+				options : {
+					sourcemap : 'auto',
+					lineNumbers : true,
+					style : 'nested',
+				},
+				files : [ {
+					expand : true,
+					cwd : './',
+					src : oc.templateSassSrc(),
+					dest : buildBase + '01_processed',
+					ext : '.css',
+					flatten: true
+				} ]
+			}
+		},
+
+		concat : {
+			template : {
+			    options: {
+					sourceMap: true,
+					sourceMapStyle: 'embed',
+				},
+				src: [ 
+					'./plugins.min.css', 
+					'<%= grunt.task.current.args[0] %>.min.css' 
+				], 
+				dest: buildBase + '03_final/css/<%= grunt.task.current.args[0] %>.min.css' 
+			},
+		},
+		
+		cssmin : {
+			options : {
+				processImport: true,
+				sourceMap: true,
+				sourceMapInlineSources: true,
+				roundingPrecision : -1,
+			},
+			//
+			// Important note regarding the source map handling:
+			//   The source map is inlined and also referenced to the original file.
+			//   If we use different src / dest folders the path information 
+			//   to the original source maps gets lost in the cssmin task. 
+			//   The workaround is to write to the './' folder, in which case the relative path information 
+			//   remains intact. We clean up later in separate 'copy:template' and 'clean:template' tasks.
+			//
+			template : {
+				files : [{
+					expand : true,
+					cwd : buildBase + '01_processed',
+					src : oc.templateCssSrc(),
+					dest : './',
+					ext : '.min.css'
+				}]
+			},
+			pluginCss : {
+				src : oc.cssSrc(),
+				dest : './plugins.min.css',
+			},
+		},
+		
+		uglify : {
+			options : {
+				sourceMap: true,
+				sourceMapIncludeSources: true,
+				mangleProperties: false,
+				mangle : {
+					except : [ 
+						'jQuery', 
+						'fancybox',
+						'bootstrapPaginator', 
+						'revolution',
+					],
+				},
+			},
+			pluginJs : {
+				src : oc.jsSrc(),
+				dest : buildBase + '03_final/js/scripts-all.min.js',
+			},
+		},
+		
+		watch: {
+			scss : {
+				files : [ '*/scss/*.scss' ],
+				tasks : [ 'template', 'combine', 'deploy' ],
+			},	
+			plugins : {
+				files : [ oc.cssSrc(), oc.jsSrc() ],
+				tasks : [ 'plugins', 'combine', 'deploy' ],
+			},
+		},
+	});
+}
+
+_gruntRegisterTasks = function() {
+
+	grunt.registerTask('default', [ 
+		'clean',
+		'template',
+		'plugins',
+		'combine',
+		'deploy',
+	]);
+	
+	grunt.registerTask('template', [ 
+		'sass',
+		'cssmin:template',
+	]);
+	
+	grunt.registerTask('plugins', [ 
+		'cssmin:pluginCss',
+		'uglify:pluginJs',
+		
+	]);
+	
+	grunt.registerTask('combine', oc.templateConcatTasks().concat(['copy:template', 'clean:template']));
+	
+	grunt.registerTask('deploy', function() {
+		if (grunt.file.expand(oc.deployTarget + '*').length) { 
+			grunt.task.run( [
+				'copy:pluginDeps',
+				'copy:deploy',
+			] );
+		} else {
+			grunt.log.errorlns('Deploy target CSS folder ' + oc.deployTarget + ' not found, skipping deploy!');
+		}
+	});	
+}	
+
+_showImports = function() {
+	
+	console.log('\nLoaded path elements from OpenCms modules');
 	console.log('- Sass');
 	for (var i in sassSrc) { console.log("    " + sassSrc[i]) };
 	
@@ -49,8 +255,26 @@ exports.showImports = function() {
 	console.log('\n- Resources');
 	for (var i in resources) { console.log("    " + resources[i]) };
 	console.log();
+	
+	console.log('\n- Deploy target folder: ' + deployTarget + "\n");	
 
-},
+}
+
+exports.sassSrc = function () {
+	return sassSrc;
+}
+
+exports.cssSrc = function () {
+	return cssSrc;
+}
+
+exports.jsSrc = function () {
+	return jsSrc;
+}
+
+exports.resources = function () {
+	return resources;
+}
 
 exports.templateSassSrc = function () {
 	for (i=0; i<templates.length; i++) {
@@ -72,4 +296,3 @@ exports.templateConcatTasks = function () {
 	}
 	return templateConcatTasks;
 }
-
--- org.opencms.apollo.template.basics/Gruntparts.js
+++ org.opencms.apollo.template.basics/Gruntparts.js
@@ -1,37 +1,49 @@
 
 var mf = module.filename.substring(0, module.filename.length - 'Gruntparts.js'.length);
-exports.mf = mf,
+
+var msegs = mf.split('/');
+var module = msegs.slice(-1);
+var repo = msegs.slice(-2, -1);
+var rf = repo + '/' + module + '/';
+
+exports.mf = mf;
+exports.rf = rf;
+exports.repo = repo;
+exports.module = module;
 
 exports.templates = [ 
 	'style-blue', 
 	'style-red'
 ];
 	
-exports.sassSrc = [],
+exports.deployTarget = process.env.OCMOUNT + '/system/modules/org.opencms.apollo.template.basics/resources/';
 
-exports.baseTemplateCssSrc = mf + 'plugins/unify/css/style.css',
+exports.sassSrc = [
+	mf + 'scss/*.scss', 
+];
 
 exports.cssSrc = [		
+	mf + 'plugins/bootstrap/css/bootstrap.css',
+	mf + 'plugins/unify/css/style.css',	
 	mf + 'plugins/unify/css/fonts.css',
 	mf + 'plugins/unify/css/headers/header-default.css',
 	mf + 'plugins/unify/css/footers/footer-default.css',
-	mf + 'plugins/bootstrap/css/bootstrap.min.css',
 	mf + 'plugins/line-icons/line-icons.css',
 	mf + 'plugins/animate.css',
-	mf + 'plugins/font-awesome/css/font-awesome.min.css',
+	mf + 'plugins/font-awesome/css/font-awesome.css',
 	mf + 'plugins/parallax-slider/css/parallax-slider.css',
 	mf + 'plugins/owl-carousel/owl-carousel/owl.carousel.css',
 	mf + 'plugins/sky-forms-pro/skyforms/css/sky-forms.css',
 	mf + 'plugins/sky-forms-pro/skyforms/custom/custom-sky-forms.css',
 	mf + 'plugins/photoswipe/photoswipe.css',
-	mf + 'plugins/photoswipe/default-skin/default-skin.css' 
+	mf + 'plugins/photoswipe/default-skin/default-skin.css',
 ],
 
 exports.jsSrc = [		
-	mf + 'plugins/jquery/jquery.min.js',
-	mf + 'plugins/jquery/jquery-migrate.min.js',
-	mf + 'plugins/bootstrap/js/bootstrap.min.js',
-	mf + 'plugins/bootstrap-paginator/bootstrap-paginator.min.js',
+	mf + 'plugins/jquery/jquery.js',
+	mf + 'plugins/jquery/jquery-migrate.js',
+	mf + 'plugins/bootstrap/js/bootstrap.js',
+	mf + 'plugins/bootstrap-paginator/bootstrap-paginator.js',
 	mf + 'plugins/back-to-top.js',
 	mf + 'plugins/smoothScroll.js',
 	mf + 'plugins/parallax-slider/js/modernizr.js',
@@ -40,11 +52,11 @@ exports.jsSrc = [
 	mf + 'plugins/unify/js/app.js',
 	mf + 'plugins/photoswipe/photoswipe.min.js',
 	mf + 'plugins/photoswipe/photoswipe-ui-default.js',
-	mf + 'plugins/unify/js/apollo.js'
+	mf + 'plugins/unify/js/apollo.js',
 ],
 
 exports.resources = [
 	mf + 'plugins/photoswipe/default-skin/*.svg', 
 	mf + 'plugins/photoswipe/default-skin/*.png', 
-	mf + 'plugins/photoswipe/default-skin/*.gif'
+	mf + 'plugins/photoswipe/default-skin/*.gif',
 ]
