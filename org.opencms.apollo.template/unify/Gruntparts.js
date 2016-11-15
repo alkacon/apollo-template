650ce45f592ebde2d95bf0f174c3ac030ba6785f
Benjamin Feldberg <b.feldberg@alkacon.com>
2016-11-15 15:46:09 +0100

    Removed unify CSS from build. Moved portions of SCSS to other file.

--- org.opencms.apollo.template/unify/Gruntparts.js
+++ org.opencms.apollo.template/unify/Gruntparts.js
@@ -3,13 +3,10 @@ var mf = module.filename.substring(0, module.filename.length - 'Gruntparts.js'.l
 exports.mf = mf;
 
 exports.cssSrc = [		
-	mf + 'css/style.css',	
-	mf + 'css/fonts.css',
-	// mf + 'css/headers/header-default.css',
-	mf + 'css/footers/footer-default.css',
 ],
 
 exports.jsSrc = [		
 	mf + 'js/app.js',
 	mf + 'js/unify.js',
 ]
+
