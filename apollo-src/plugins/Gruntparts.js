6022311b234696f0f45388a6aded47af35303766
aKandzior <a.kandzior@alkacon.com>
2016-12-19 18:16:03 +0100

    Added shariff resources to Grunt build.

--- apollo-src/plugins/Gruntparts.js
+++ apollo-src/plugins/Gruntparts.js
@@ -20,28 +20,30 @@
 var mf = module.filename.substring(0, module.filename.length - 'Gruntparts.js'.length);
 exports.mf = mf;
 
-exports.cssSrc = [		
-	mf + 'line-icons/line-icons.css',
-	mf + 'animate/animate.css',
-	mf + 'parallax-slider/css/parallax-slider.css',
-	mf + 'owl-carousel/owl.carousel.css',
-	mf + 'sky-forms-pro/skyforms/css/sky-forms.css',
-	mf + 'sky-forms-pro/skyforms/custom/custom-sky-forms.css',
-	mf + 'photoswipe/photoswipe.css',
-	mf + 'photoswipe/default-skin/default-skin.css',
+exports.cssSrc = [        
+    mf + 'line-icons/line-icons.css',
+    mf + 'animate/animate.css',
+    mf + 'parallax-slider/css/parallax-slider.css',
+    mf + 'owl-carousel/owl.carousel.css',
+    mf + 'sky-forms-pro/skyforms/css/sky-forms.css',
+    mf + 'sky-forms-pro/skyforms/custom/custom-sky-forms.css',
+    mf + 'photoswipe/photoswipe.css',
+    mf + 'photoswipe/default-skin/default-skin.css',
+    mf + 'shariff/shariff.min.css',
 ];
 
-exports.jsSrc = [		
-	mf + 'bootstrap-paginator/bootstrap-paginator.js',
-	mf + 'parallax-slider/js/modernizr.js',
-	mf + 'parallax-slider/js/jquery.cslider.js',
-	mf + 'owl-carousel/owl-carousel/owl.carousel.js',
-	mf + 'photoswipe/photoswipe.min.js',
-	mf + 'photoswipe/photoswipe-ui-default.js',
+exports.jsSrc = [        
+    mf + 'bootstrap-paginator/bootstrap-paginator.js',
+    mf + 'parallax-slider/js/modernizr.js',
+    mf + 'parallax-slider/js/jquery.cslider.js',
+    mf + 'owl-carousel/owl-carousel/owl.carousel.js',
+    mf + 'photoswipe/photoswipe.min.js',
+    mf + 'photoswipe/photoswipe-ui-default.js',
+    mf + 'shariff/shariff.min.js',
 ];
 
 exports.resources = [
-	mf + 'photoswipe/default-skin/*.svg', 
-	mf + 'photoswipe/default-skin/*.png', 
-	mf + 'photoswipe/default-skin/*.gif',
+    mf + 'photoswipe/default-skin/*.svg', 
+    mf + 'photoswipe/default-skin/*.png', 
+    mf + 'photoswipe/default-skin/*.gif',
 ];
