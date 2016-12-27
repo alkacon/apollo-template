0d7f82cb0426f464fe620995a21d8929782e46d5
aKandzior <a.kandzior@alkacon.com>
2016-12-27 11:14:28 +0100

    Continued working on the init-messages - SCSS.

--- apollo-src/js/script-app.js
+++ apollo-src/js/script-app.js
@@ -68,14 +68,14 @@ var App = function() {
     // Smooth scrolling to anchor links
     function handleSmoothScrolling() {
 
-        jQuery('a[href*="#"]:not([href="#"]):not(.nav-toggle)')
+        jQuery('a[href*="#"]:not([href="#"]):not([data-toggle])')
             .click(function() {
 
                 if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    var target = jQuery(this.hash);
+                    target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
                     if (target.length) {
-                        $('html, body').animate({
+                        jQuery('html, body').animate({
                             scrollTop : target.offset().top
                         }, 1000);
                         return false;
